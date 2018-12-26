0971fa9270a518433185ecfeabf1eab70919ffa9
Xavier <xavier@skillup.co>
2018-12-26 14:47:39 +0100

    update package and generate artist template pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,40 @@
 module.exports = {
-    siteMetadata: {
-      title: `Manakin Sky dancer Rocks !`
-    },
-    plugins: [
-      `gatsby-plugin-sass`,
-      `gatsby-transformer-remark`,
-      {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: `l6zoou2d4e5q`,
-          accessToken: `9dc9304c61118a176c123a46dbe135dae690219c975be43e115b8bd10aa2a2b2`,
-        },
+  siteMetadata: {
+    title: `Manakin Sky dancer Rocks !`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `l6zoou2d4e5q`,
+        accessToken: `9dc9304c61118a176c123a46dbe135dae690219c975be43e115b8bd10aa2a2b2`,
       },
-      {
-        resolve: `gatsby-plugin-favicon`,
-        options: {
-          logo: "./src/resources/favicon.png",
-          injectHTML: true,
-          icons: {
-            android: true,
-            appleIcon: true,
-            appleStartup: true,
-            coast: false,
-            favicons: true,
-            firefox: true,
-            twitter: false,
-            yandex: false,
-            windows: false
-          }
-        }
-      }
-    ]
-  }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: 'src/resources/favicon.png',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `lora\:300,300i,700`,
+          `open sans\:400,400i,700`,
+          `fira mono\:400,400i,700`,
+        ],
+      },
+    },
+  ],
+};
