1f04f2fc8b9d602c9198ecbc4947ad11ac2ea002
Xavier <xajouppe@gmail.com>
2019-01-03 23:40:36 +0100

    little commit Anne

--- src/components/artitstsListForAgenda/index.js
+++ src/components/artitstsListForAgenda/index.js
@@ -21,7 +21,7 @@ export default props => (
       <div styles={styles} className="artist-list-for-agenda">
         {data.allContentfulArtists.edges.map(artist => (
           <Link
-            to={`agenda/${sanitizeName(artist.node.name)}`}
+            to={`agenda/${props.active === artist.node.name ? '' : sanitizeName(artist.node.name)}`}
             key={artist.node.name}
             className={props.active === artist.node.name ? 'active' : ''}
           >
