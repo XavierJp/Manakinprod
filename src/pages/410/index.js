3cabeaba986c557fd72489ae8a371cb2a4b417af
xavierJp <xajouppe@gmail.com>
2020-01-16 12:53:44 +0100

    add a 410 and update redirect

---
+++ src/pages/410/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.scss';
+import Header from '../../uiComponents/header';
+import Layout from '../../components/layout';
+import Link from '../../uiComponents/link';
+
+export default () => (
+  <Layout>
+    <Header />
+    <div className="error-410">
+      <span role="img" aria-label="Sad-face" className="smiley-410">
+        😭
+      </span>
+      <p>Oh non ! Il semblerait que cette page ait été supprimée...</p>
+      <Link to="/" className="back button">
+        <p>Retourner à la page d'accueil</p>
+      </Link>
+    </div>
+  </Layout>
+);
